adfefd0b80e07764c950215305bfbf2c40fc4151
Serhii Turlak <turlakserhii@gmail.com>
2025-08-18 16:11:44 +0300

    Stop creating tables eash startup just based on models. With Alembic there is no need in that anymore

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
-from app.db.base import Base
-from app.db.session import engine
 from app.api import auth, users
 from app.core.middleware import AuthMiddleware
-
-# Gonne replace with Alembic
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Messenger API")
 app.add_middleware(
